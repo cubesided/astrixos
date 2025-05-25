9dbf9a5cc5cbb4ae97eb275fcb45f93cda26b24f
Actium <127707818+ugman21@users.noreply.github.com>
2025-05-25 12:00:11 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,6 @@ if [ "$EUID" -ne 0 ]
             wait 2
             fi
     else
-    clear
     echo installing packages, please wait.....
     sudo apt --assume-yes -qq install sway xorg xwayland gnome-terminal
     sudo apt --assume-yes -qq install waybar rofi git
